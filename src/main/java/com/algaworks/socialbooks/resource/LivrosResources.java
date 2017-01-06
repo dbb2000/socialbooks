9f70bceb78a59828db5beb9623b5eb3bd234c0f9
Davi Bariani Boin <t29187305801@dasa.com.br>
2017-01-06 16:26:19 -0200

    adicionando cache

--- src/main/java/com/algaworks/socialbooks/resource/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resource/LivrosResources.java
@@ -2,10 +2,12 @@ package com.algaworks.socialbooks.resource;
 
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,8 +47,10 @@ public class LivrosResources {
 	@RequestMapping(value = "/{id}" , method = RequestMethod.GET)
 	public ResponseEntity<?> buscar(@PathVariable("id") Long id){
 		
-		Livro livro = livrosService.buscar(id);		
-		return ResponseEntity.status(HttpStatus.OK).body(livro);
+		Livro livro = livrosService.buscar(id);				
+		CacheControl cacheControl = CacheControl.maxAge(20, TimeUnit.SECONDS);
+		
+		return ResponseEntity.status(HttpStatus.OK).cacheControl(cacheControl).body(livro);
 	}
 	
 	@RequestMapping(value = "/{id}" , method = RequestMethod.DELETE)
